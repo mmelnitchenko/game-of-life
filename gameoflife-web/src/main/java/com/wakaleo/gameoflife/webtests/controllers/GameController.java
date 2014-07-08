fae16542cfdda9e88c02a0eb3e5d7b98e107baf1
Myriam Melnitchenko <mmelnitchenko@sii.fr>
2014-07-08 17:08:25 +0200

    test

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.wakaleo.gameoflife.domain.Universe;
+/*test pour pusher une modif sur github*/
 
 @Controller
 @RequestMapping("/game")
